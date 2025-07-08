899802521c8f643a7cee30bc6cce62d7f8a70ecb
tejass34555ttt <tejasshinde4545k@gmail.com>
2025-07-08 09:59:22 +0530

    first commit

---
+++ controllers/auth_controllers.js
@@ -0,0 +1,32 @@
+import {Client,Account,ID,Functions} from "appwrite"
+
+const client = new Client()
+.setEndpoint('http://89.116.21.143/v1') // Your API Endpoint
+.setProject('68492a7c0008fcc0cbc2')              // Your project ID
+
+const account = new Account(client);
+
+// complete verification for the client
+export const updateVerification = async (userId, secret) => {
+    try{
+       const response=await account.updateVerification(userId, secret);
+         console.log(response); // Success
+            return response;
+    }
+    catch(error){
+        console.log(error); // Failure
+        throw error;
+    }
+}
+
+// update and reset the password
+export const updateNewPassword = async (userId, secret, password,password_confirm) => {
+    try {
+        const response = await account.updateRecovery(userId, secret, password,password_confirm);
+        console.log(response); // Success
+        return response;
+    } catch (error) {
+        console.log(error); // Failure
+        throw error;
+    }
+};
